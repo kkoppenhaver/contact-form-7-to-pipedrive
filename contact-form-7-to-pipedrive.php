76e6187dc5ff7a2f9b6f42a9cfec0c81794b670a
Keanan Koppenhaver <keanan@howtogeek.com>
2017-09-30 13:06:28 -0700

    Initial plugin header commit

---
+++ contact-form-7-to-pipedrive.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Contact Form 7 to Pipedrive
+Plugin URI: https://wordpress.org/plugins/contact-form-7-to-pipedrive/
+Description: Turns Contact Form 7 leads into Pipedrive Deals
+Version: 0.1.0
+Author: Alpha Particle
+Author URI: http://alphaparticle.com
+Text Domain: cf7-to-pipedrive
+*/
